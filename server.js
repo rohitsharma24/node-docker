52bacf838b2b09665d6d0100455ddac8b974b83b
Rohit <rohit.sharma.2495@outlook.com>
2018-07-24 23:39:38 +0530

    add PORT  as const

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send('hello world');
 }); 
 
-app.listen(3000, (err) => {
+app.listen(PORT, (err) => {
     if(err) {
         console.log('error starting server');
     }
